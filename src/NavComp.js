c5732f7fc6620e9f839fb064ecc47e1a3f392cb0
kelly_maass <kmaass77627@gmail.com>
2024-11-30 11:29:06 -0600

    done

--- src/NavComp.js
+++ src/NavComp.js
@@ -26,40 +26,8 @@ function NavComp() {
             <Link id="a" to="/location">Location</Link>
             <Link id="a" to="/about">About</Link>
             </div>
-              {/* <Navbar.Toggle aria-controls="basic-navbar-nav" /> */}
-                {/* <Navbar.Collapse id="basic-navbar-nav"> */}
-                  {/* <Nav id="links" className="me-auto"> */}
-                  {/* <Nav.Link  href={<BasicInfo></BasicInfo>} >Home</Nav.Link> */}
-                  {/* <Nav.Link href="#link">Link</Nav.Link> */}
-                  {/* <NavDropdown title="Dropdown" id="basic-nav-dropdown"> */}
-                  {/* <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item> */}
-                  {/* <NavDropdown.Item href="#action/3.2"> */}
-                {/* Another action */}
-              {/* </NavDropdown.Item> */}
-              {/* <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item> */}
-              {/* <NavDropdown.Divider /> */}
-              {/* <NavDropdown.Item href="#action/3.4"> */}
-                {/* Separated link */}
-              {/* </NavDropdown.Item> */}
-            {/* </NavDropdown> */}
-          {/* </Nav> */}
-          
-          {/* <Form inline> */}
-        {/* <Row> */}
-          {/* <Col xs="auto"> */}
-            {/* <Form.Control
-              type="text"
-              placeholder="Search"
-              className=" mr-sm-2"
-            />
-          </Col>
-          <Col xs="auto">
-            <Button type="submit">Submit</Button>
-          </Col>
-        </Row>
-      </Form> */}
-
-        {/* </Navbar.Collapse> */}
+            
+        
       </Container>
     </Navbar>
          
--- src/Wine.js
+++ src/Wine.js
@@ -7,8 +7,8 @@ function Wine({ wine }) {
         <div id="wine">
             <h3 id="h3">{wine.name}</h3>
         {/* <h5 id="h5">{item.ingredients}</h5> */}
-        <img id="wineimg" src={wine.image} alt="wine"></img>
-        <h3 id="price">{wine.price}</h3>
+            <img id="wineimg" src={wine.image} alt="wine"></img>
+            <h3 id="price">{wine.price}</h3>
 
         </div>
     )
